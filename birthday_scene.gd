de807fce1a30c7aa8fec43d8cc75d9d2f99867de
ddash <ddash@car-fluent.com>
2024-07-15 13:11:55 +0200

    Looks working

--- birthday_scene.gd
+++ birthday_scene.gd
@@ -19,10 +19,15 @@ var greeting_vertical_offset = 100
 var mia_target_position: Vector2
 var dima_target_position: Vector2
 
-var moving: int = 0
+var is_game_started: bool = false
+
+var moving_targets: int = 0
+var walk_started: bool = false
 var greeting: bool = false
 
+var mia_intro_started: bool = false
 var mia_intro_done: bool = false
+
 var dima_intro_started: bool = false
 var dima_intro_done: bool = false
 
@@ -30,6 +35,12 @@ var dima_intro_done: bool = false
 func _ready():
 	var window_size = get_viewport_rect().size
 	print("Game Window Size: ", window_size.x, "x", window_size.y)
+	#_on_size_changed()
+
+	# Connect the resize signal to handle window resizing
+	#get_viewport().connect("size_changed", self._on_size_changed)
+	
+	$Start.connect("pressed", self.on_start_press)
 	
 	greeting_timer.connect("timeout", self.on_greeting_finished)
 	mia_intro_timer.connect("timeout", self.on_mia_intro_finished)
@@ -38,25 +49,27 @@ func _ready():
 	# Set the target position to the center of the screen
 	mia_target_position = Vector2(window_size.x / 2 - char_width, window_size.y - greeting_vertical_offset)
 	dima_target_position = Vector2(window_size.x / 2 + char_width, window_size.y - greeting_vertical_offset)
-	
-	mia_start_intro()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if !mia_intro_done:
+	if !is_game_started:
+		pass
+	elif !mia_intro_started:
+		mia_start_intro()
+	elif !mia_intro_done:
 		pass
 	elif !dima_intro_started:
 		dima_start_intro()
 	elif !dima_intro_done:
 		pass
-	elif moving == 0:
+	elif !walk_started:
 		# Start the walk animation and movement
+		walk_started = true
 		mia_anim_player.play("walk")
 		dima_anim_player.play("walk")
 		$Walk.play()
-		moving = 2
-	elif moving > 0:
+		moving_targets = 2
+	elif moving_targets > 0:
 		move_towards_target(mia, mia_target_position, delta)
 		move_towards_target(dima, dima_target_position, delta)
 	elif !greeting:
@@ -77,9 +90,10 @@ func move_towards_target(player, target_position, delta):
 	if player.position.distance_to(target_position) < walk_speed * delta:
 		player.position = target_position
 		player.get_node("AnimationPlayer").stop()
-		moving -= 1
+		moving_targets -= 1
 		
 func mia_start_intro():
+	mia_intro_started = true
 	mia_anim_player.play("m_bday")
 	$MiaIntro.play()
 	mia_intro_timer.start()
@@ -104,3 +118,16 @@ func on_dima_intro_finished():
 	dima_intro_done = true
 	dima_anim_player.stop()
 	$DimaIntro.stop()
+	
+func on_start_press():
+	is_game_started = true
+	$Start.hide()
+	
+#func _on_size_changed():
+	#var window_size = get_viewport_rect().size
+	#print("Game Window Size: ", window_size.x, "x", window_size.y)
+	#
+	#print(dima.position.x, window_size.x)
+#
+	## Reposition the asset right after the viewport
+	#dima.position.x = window_size.x
